87bb2284464fdc56c8458fe65b672ffaefcc303e
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-04-12 22:27:02 +0300

    chore: tune some settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	css: {
-		modules: {
-			localsConvention: 'camelCase',
-		},
-		preprocessorOptions: {
-			scss: {
-				additionalData: `@use "sass:color";`,
-			},
-		},
-	},
-	build: {
-		minify: 'esbuild',
-	},
-	base: '', // здесь пусто?
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "sass:color";`,
+      },
+    },
+  },
+  build: {
+    minify: 'esbuild',
+  },
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
