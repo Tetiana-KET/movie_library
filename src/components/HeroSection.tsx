960017dfbcdd167fe71413fab517c1657c2e3757
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-04-27 12:26:14 +0300

    refactor: change img extension to webp

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 export const HeroSection = () => {
   return (
     <section className="hero">
-      <img src="/hero-img.png" alt="Hero Banner" />
+      <img width={512} height={327} src="/hero-img.webp" alt="Hero Banner" loading="eager" />
       <h1>
         Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle
       </h1>
