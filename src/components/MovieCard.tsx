a9d760df2748ee3bdcb6f179800a9c9365accf0a
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-04-28 10:34:02 +0300

    refactor: add semantic tags, implement aspect ratio for movie posters

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -12,35 +12,40 @@ export const MovieCard = ({ movie }: MovieCard) => {
   const genreList = genre_ids.map((id) => GENRES_MAP[id]).filter(Boolean);
 
   return (
-    <li className="movie-card">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-        alt={`a poster for movie: ${title}`}
-        onError={(e) => {
-          const target = e.currentTarget;
-          target.onerror = null;
-          target.src = '/no-poster.webp';
-        }}
-      />
-      <div className="mt-4">
-        <h3>{title}</h3>
-        <div className="content">
-          <div className="rating">
-            <StarIcon />
-            <p>{vote_average.toFixed(1) || 'N/A'}</p>
-          </div>
-          <span>•</span>
-
-          {genreList.length && (
-            <div className="genres">
-              {genreList.slice(0, 2).map((name) => (
-                <span key={name}>{name}</span>
-              ))}
-            </div>
-          )}
-          <p className="year">{release_date.split('-')[0] || 'N/A'}</p>
+    <li>
+      <figure className="movie-card">
+        <div className="ratio-box">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+            alt={`a poster for movie: ${title}`}
+            style={{ objectPosition: 'center 25%' }}
+            onError={(e) => {
+              const target = e.currentTarget;
+              target.onerror = null;
+              target.src = '/no-poster.webp';
+            }}
+          />
         </div>
-      </div>
+        <figcaption className="mt-4">
+          <h3>{title}</h3>
+          <div className="content">
+            <div className="rating">
+              <StarIcon />
+              <p>{vote_average.toFixed(1) || 'N/A'}</p>
+            </div>
+            <span>•</span>
+
+            {genreList.length && (
+              <div className="genres">
+                {genreList.slice(0, 2).map((name) => (
+                  <span key={name}>{name}</span>
+                ))}
+              </div>
+            )}
+            <p className="year">{release_date.split('-')[0] || 'N/A'}</p>
+          </div>
+        </figcaption>
+      </figure>
     </li>
   );
 };
