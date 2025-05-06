0844ace97b0f2cb99f60a5b54e1b0f69c035c637
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-05-06 13:36:39 +0300

    refactor: improve genre age rating

--- src/utils/getAgeRating.ts
+++ src/utils/getAgeRating.ts
@@ -1,14 +1,14 @@
-import { AGE_RATINGS, AgeRatingInfo } from '@/consts/AGE_RATINGS';
+import { MOVIE_AGE_RATINGS, AgeRatingInfo } from '@/consts/MOVIE_AGE_RATINGS';
 
 const familyFriendlyGenreIds = [14, 16, 35, 10751];
-const intenseGenreIds = [27, 53, 80, 9648, 10752, 10770];
+const intenseGenreIds = [80, 9648, 10752, 10770];
 const moderateGenreIds = [28, 12, 18, 36, 37, 99, 878, 10402, 10749];
-
+const adultGenres = [27, 53];
 export const getAgeRating = (adult: boolean, genreIds: number[]): AgeRatingInfo => {
-  if (adult) return AGE_RATINGS['18+'];
-  if (genreIds.some((id) => familyFriendlyGenreIds.includes(id))) return AGE_RATINGS.Family;
-  if (genreIds.some((id) => intenseGenreIds.includes(id))) return AGE_RATINGS['16+'];
-  if (genreIds.some((id) => moderateGenreIds.includes(id))) return AGE_RATINGS['PG-13'];
+  if (adult || genreIds.some((id) => adultGenres.includes(id))) return MOVIE_AGE_RATINGS['18+'];
+  if (genreIds.some((id) => familyFriendlyGenreIds.includes(id))) return MOVIE_AGE_RATINGS.Family;
+  if (genreIds.some((id) => intenseGenreIds.includes(id))) return MOVIE_AGE_RATINGS['16+'];
+  if (genreIds.some((id) => moderateGenreIds.includes(id))) return MOVIE_AGE_RATINGS['PG-13'];
 
-  return AGE_RATINGS['PG-13'];
+  return MOVIE_AGE_RATINGS['PG-13'];
 };
