5407e4cc05bee2fb6d513b5676159a4f7f601d17
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-05-13 16:08:30 +0300

    refactor: move searchQuery state to Loader

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,24 +5,31 @@ import { Pagination } from '@/components/Pagination';
 import { Spinner } from '@/components/ui/Spinner';
 import { MoviesSection } from '@/components/MoviesSection';
 import { CategoriesSection } from '@/components/categories/CategoriesSection';
-import { useDebounce } from '@/hooks/useDebounce';
 import { useRestoreScrollPositionOnPop } from '@/hooks/useRestoreScrollPositionOnPop';
 import { useTrendingLoader } from '@/hooks/useTrendingLoader';
 import { useGenresLoader } from '@/hooks/useGenresLoader';
 import { useMediaLoader } from '@/hooks/useMediaLoader';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { scrollToSection } from '@/utils/scrollToSection';
 import { useSyncPagination } from '@/hooks/useSyncPagination';
 
 export const MainPage = () => {
   useGenresLoader();
-  const [searchQuery, setSearchQuery] = useState('');
-  const debouncedQuery = useDebounce(searchQuery, 700);
 
   const { currentPage, setCurrentPage } = useSyncPagination();
   const { trendingMovies, heroPostersPaths } = useTrendingLoader();
-  const { movieList, totalPages, errorMessage, isLoading, selectedCategory, setSelectedCategory, sortBy, setSortBy } =
-    useMediaLoader(debouncedQuery, currentPage, setCurrentPage);
+  const {
+    movieList,
+    totalPages,
+    errorMessage,
+    isLoading,
+    selectedCategory,
+    setSelectedCategory,
+    sortBy,
+    setSortBy,
+    searchQuery,
+    setSearchQuery,
+  } = useMediaLoader(currentPage, setCurrentPage);
 
   const moviesSectionRef = useRef<HTMLDivElement | null>(null);
 
@@ -57,7 +64,6 @@ export const MainPage = () => {
         ref={moviesSectionRef}
         sortBy={sortBy}
         setSortBy={setSortBy}
-        debouncedQuery={debouncedQuery}
       />
       {movieList.length && (
         <Pagination
