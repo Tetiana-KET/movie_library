e94f6a6892d63227568a611c1234ce1e46583a39
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-05-15 15:03:35 +0300

    refactor(search): set query on submit, add reset button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchIcon } from './ui/SearchIcon';
+import { scrollToSection } from '@/utils/scrollToSection';
+import { CrossIcon } from './ui/CrossIcon';
 
 interface SearchProps {
   searchQuery: string;
   setSearchQuery: (query: string) => void;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  moviesSectionRef: React.RefObject<HTMLDivElement | null>;
 }
 
-const Search = ({ searchQuery, setSearchQuery }: SearchProps) => {
+const Search = ({ searchQuery, setSearchQuery, setCurrentPage, moviesSectionRef }: SearchProps) => {
+  const [inputValue, setInputValue] = useState('');
+
+  // insert query from props into the input, on mounting
+  useEffect(() => {
+    setInputValue(searchQuery);
+  }, [searchQuery]);
+
+  // Tracking text deletion - auto-reset request
+  useEffect(() => {
+    if (inputValue === '' && searchQuery !== '') {
+      setSearchQuery('');
+    }
+  }, [inputValue, searchQuery]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchQuery(inputValue.trim());
+    setCurrentPage(1);
+
+    setTimeout(() => {
+      scrollToSection(moviesSectionRef);
+    }, 0);
+  };
+
+  const handleReset = () => {
+    if (inputValue) {
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="search">
+    <form onSubmit={handleSubmit} className="search">
       <div>
         <SearchIcon />
         <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           type="text"
           placeholder="Search through hundreds of movies online"
-          value={searchQuery}
-          onChange={(e) => {
-            setSearchQuery(e.target.value);
-          }}
           className="placeholder-ellipsis w-full overflow-hidden text-ellipsis whitespace-nowrap"
         />
+        <button
+          type="button"
+          className="button p-0 w-[30px] h-[30px] hover:bg-light-100/20 text-accent flex items-center justify-center"
+          onClick={handleReset}
+          disabled={!Boolean(inputValue.trim())}
+        >
+          <CrossIcon />
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
